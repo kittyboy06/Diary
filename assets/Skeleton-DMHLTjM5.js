4516ad22955853ce9ac0c17c09fb2d5be693e398
Kittyboy06 <afsalahmed2006@gmail.com>
2026-01-04 01:03:54 +0530

    Deploy from manual script

---
+++ assets/Skeleton-DMHLTjM5.js
@@ -0,0 +1,1 @@
+import{j as a}from"./index-DQ_HIcJs.js";function s({className:e,...t}){return a.jsx("div",{className:`animate-pulse bg-neutral-200 dark:bg-slate-700 rounded-md ${e}`,...t})}export{s as S};
---
+++ registerSW.js
@@ -0,0 +1,1 @@
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/Diary/sw.js', { scope: '/Diary/' })})}
